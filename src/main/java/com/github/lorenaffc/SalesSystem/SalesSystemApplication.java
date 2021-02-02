4681f3060e90409c034542a90f2bb4eb7095665f
LorenaFFC <fernandescunhalorena3@gmail.com>
2021-02-02 15:52:45 -0300

    Criacao do Projeto

---
+++ src/main/java/com/github/lorenaffc/SalesSystem/SalesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.lorenaffc.SalesSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesSystemApplication.class, args);
+	}
+
+}
