59bfbcd60fdc6bbd620cef014606f1ff251a37ab
LorenaFFC <fernandescunhalorena3@gmail.com>
2021-02-03 14:45:17 -0300

    Tratando Erros da API

--- src/main/java/com/github/lorenaffc/SalesSystem/rest/ClienteController.java
+++ src/main/java/com/github/lorenaffc/SalesSystem/rest/ClienteController.java
@@ -1,5 +1,7 @@
 package com.github.lorenaffc.SalesSystem.rest;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -39,7 +41,7 @@ public class ClienteController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar(@RequestBody Cliente cliente) {
+	public Cliente salvar(@RequestBody @Valid Cliente cliente) {
 		return clienteRepository.save(cliente);
 
 	}
